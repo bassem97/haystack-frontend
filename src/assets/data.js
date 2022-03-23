387d13d4425ac487b6891111edb7087b409d25e8
BASSEM <bassem.jadoui@esprit.tn>
2022-03-24 00:28:21 +0100

    carousel khra 💩💩

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,18 +1,25 @@
 export const sliderItems = [
     {
-      id: 2,
+      id: 1,
       img: "https://i.ibb.co/DG69bQ4/2.png",
-      title: "AUTUM COLLECTION",
+      title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
-      id: 3,
+      id: 2,
       img: "https://i.ibb.co/cXFnLLV/3.png",
-      title: "LOUNGEWEA LOVE",
+      title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
     },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
   ];
 
   export const categories = [
@@ -29,7 +36,7 @@ export const sliderItems = [
     {
       id: 3,
       img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      title: "BLUE BLACK JACKET",
+      title: "LIGHT JACKETS",
     },
   ];
 
