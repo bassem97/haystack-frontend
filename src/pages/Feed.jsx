abf613441f78a69747e2a2281de8d013088a9568
SaddemAmine <naceuramine.saddem@esprit.tn>
2022-04-27 00:54:26 +0100

    Ya rab waffa9na

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,30 +1,59 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import InfiniteScroll from 'react-infinite-scroll-component'
 import Product from '../components/ProductAlt'
 
 const Feed = () => {
     const [products, setProducts] = useState([]);
+    const [step, setStep] = useState(3);
+    const [hasMore, setHasMore] = useState(true);
+    
+    const getProducts = async () => {
+        const res = await axios.get(
+            process.env.REACT_APP_API_URL + `/user/feed/624b77b7a7a94effd792c275/${step}`
+        );
+        
+        if(res.data.products.length > 0) {
+            setProducts([...products, ...res.data.products]);
+            setStep(step + 1);
+        }
+        else{
+            setHasMore(false);
+        }
 
+        console.log(products, step, hasMore)
+    };
+    
     useEffect(() => {
-        const getProducts = async () => {
-            try {
-                const res = await axios.get(
-                    process.env.REACT_APP_API_URL + "/user/feed/624b77b7a7a94effd792c275"
-                );
+        (async () => {const res = await axios.get(
+            process.env.REACT_APP_API_URL + `/user/feed/624b77b7a7a94effd792c275`
+        );
+        
+        if(res.data.products.length > 0) {
+            setProducts([...products, ...res.data.products]);
+            setStep(step + 1);
+        }
+        else{
+            setHasMore(false);
+        }
 
-                console.log(res.data.product)
-                await setProducts(res.data.product);
-                } catch (err) {
-            }
-        };
-        getProducts();
+        console.log(products, step, hasMore)})()
     }, []);
 
     return (
-        <div className="w-full py-20">
-            {products.map(product => <Product product={product} /> )}
-        </div>
+        <InfiniteScroll
+            dataLength={products.length} //This is important field to render the next data
+            next={getProducts}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            endMessage={
+                <p style={{ textAlign: 'center' }}>
+                    <b>Yay! You have seen it all</b>
+                </p>
+            }
+        >
+            {products.map((product, index) => <Product key={index} product={product} />)}
+        </InfiniteScroll>
     )
 }
 
