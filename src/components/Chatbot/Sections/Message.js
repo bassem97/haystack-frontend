c7ef7ea02547e6ccc1a0043c4e80f311a73c3ec5
Zgon <karim.mannai7@gmail.com>
2022-04-26 23:08:45 +0100

    chatbot fixing

--- src/components/Chatbot/Sections/Message.js
+++ src/components/Chatbot/Sections/Message.js
@@ -3,17 +3,18 @@ import { List, Icon, Avatar } from 'antd';
 
 function Message(props) {
 
-    const AvatarSrc = props.who ==='bot' ? <Icon type="robot" /> : <Icon type="smile" />  
+    const AvatarSrc = props.who ==='bot' ? <Icon type="robot" /> : <Icon type="smile" />
     const title = props.title
     const  description = props.description
     return (
         <List.Item style={{ padding: '1rem' }}>
-
-            <List.Item.Meta
-                avatar={<Avatar icon={AvatarSrc} />}
-                title={props.who}
-                description={props.text}
-            />
+            <div><center>
+                <List.Item.Meta
+                    title={props.who}
+                    description={props.text}
+                    style={{background: '#8997ee'}}
+                /></center>
+            </div>
         </List.Item>
 
     )
---
+++ src/pages/admin/Complaint.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// components
+
+import CardComplaint from "../../components/admin/Cards/CardComplaint.js";
+import Sidebar from "../../components/admin/Sidebar/Sidebar";
+import HeaderStats from "../../components/admin/Headers/HeaderStats";
+
+export default function Orders() {
+    return (
+        <>
+            <Sidebar />
+            <div className="relative md:ml-64 bg-blueGray-100">
+                <div className="flex flex-wrap mt-4">
+                    <div className="w-full mb-12 px-4">
+                        <CardComplaint color="dark" />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
