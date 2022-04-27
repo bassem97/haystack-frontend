9d63ab8771a84369aa1739c018d2999ee127de48
SaddemAmine <naceuramine.saddem@esprit.tn>
2022-04-27 09:38:56 +0100

    🍜

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -2,20 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import InfiniteScroll from 'react-infinite-scroll-component'
 import Product from '../components/ProductAlt'
+import Spinner from '../components/Spinner'
+import {useAuthState} from "../Context";
 
 const Feed = () => {
     const [products, setProducts] = useState([]);
-    const [step, setStep] = useState(3);
+    const [step, setStep] = useState(0);
     const [hasMore, setHasMore] = useState(true);
-    
+    const userDetails = useAuthState();
+    const user = userDetails.user;
     const getProducts = async () => {
         const res = await axios.get(
-            process.env.REACT_APP_API_URL + `/user/feed/624b77b7a7a94effd792c275/${step}`
+            process.env.REACT_APP_API_URL + `/user/feed/${user._id}/${step}`
         );
-        
+
         if(res.data.products.length > 0) {
             setProducts([...products, ...res.data.products]);
-            setStep(step + 1);
+            setStep(step + 3);
         }
         else{
             setHasMore(false);
@@ -23,15 +26,15 @@ const Feed = () => {
 
         console.log(products, step, hasMore)
     };
-    
+
     useEffect(() => {
         (async () => {const res = await axios.get(
-            process.env.REACT_APP_API_URL + `/user/feed/624b77b7a7a94effd792c275`
+            process.env.REACT_APP_API_URL + `/user/feed/${user._id}`
         );
-        
+
         if(res.data.products.length > 0) {
             setProducts([...products, ...res.data.products]);
-            setStep(step + 1);
+            setStep(step + 3);
         }
         else{
             setHasMore(false);
@@ -45,12 +48,14 @@ const Feed = () => {
             dataLength={products.length} //This is important field to render the next data
             next={getProducts}
             hasMore={hasMore}
-            loader={<h4>Loading...</h4>}
+            loader={<div className="my-12 w-full flex justify-center"><Spinner /></div>}
             endMessage={
                 <p style={{ textAlign: 'center' }}>
-                    <b>Yay! You have seen it all</b>
+                    <b>!أرجع غدوة</b>
                 </p>
             }
+
+            className="px-80"
         >
             {products.map((product, index) => <Product key={index} product={product} />)}
         </InfiniteScroll>
